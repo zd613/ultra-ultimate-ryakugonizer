ff5aace0f171d12abf1144f293ac847d93e79e9d
ame <25921047+zd613@users.noreply.github.com>
2021-12-07 00:53:40 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial/>
+  <div>
+    <textarea
+      v-model="inputText"
+      class="border-2 border-purple-200 rounded-xl"
+      placeholder="文字入力"
+      rows="10"
+      cols="40"
+    ></textarea>
+
+    <div class="border-2 border-purple-200 rounded-xl">
+      {{ outputText }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      inputText: '',
+    }
+  },
+
+  computed: {
+    outputText(): string {
+      return this.inputText
+    },
+  },
+})
 </script>
